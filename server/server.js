16f2a9d059cf5a07a87d9790498ea03a10214220
Phodal Huang <h@phodal.com>
2014-08-16 22:41:48 +0800

    Add the Method Support

--- server/server.js
+++ server/server.js
@@ -6,12 +6,16 @@ var QueryData = require('./QueryData.js');
 DBHelper.initDB();
 
 server.on('request', function(req, res) {
-    if (req.headers['GET'] !== 0) {
-        res.setOption('Accept', 'application/json');
+    console.log(req.method);
+    if (req.method == 'GET') {
         DBHelper.urlQueryData(req.url, function(result) {
             QueryData.returnJSON(result, res);
         });
-    } else {
+    } else if (req.method == 'POST' || req.method == 'PUT' || req.method == 'DELETE') {
+        res.end(JSON.stringify({
+            message: req.method +" is no support now"
+        }));
+    }else {
         res.end(JSON.stringify({
             error: "sorry"
         }));
