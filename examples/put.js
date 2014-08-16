894a46370f6bfdb7afeaa21b2d5e914a6474d56c
Phodal Huang <h@phodal.com>
2014-08-16 22:41:48 +0800

    Add the Method Support

---
+++ examples/put.js
@@ -0,0 +1,23 @@
+var coap = require('coap');
+var request = coap.request;
+
+var req = request({
+    hostname: 'localhost',
+    port:5683,
+    pathname: '',
+    method: 'PUT'
+});
+
+req.setHeader("Accept", "application/json");
+var bl = require('bl');
+
+req.on('response', function(res) {
+    res.pipe(bl(function(err, data) {
+        var json = JSON.parse(data);
+        console.log(json);
+        process.exit(0);
+    }));
+
+});
+
+req.end();
