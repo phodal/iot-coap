aedc78237c4990b53a9f1f16182bb2282640a7fd
Phodal Huang <h@phodal.com>
2014-08-19 16:56:12 +0800

    Add the PUT support

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ server.on('request', function(req, res) {
     switch(req.method){
         case "GET": request_handler.getHandler(req, res);
             break;
-        case "POST": request_handler.postHandler(req, res);
+        case "POST":
+        case "PUT": request_handler.postHandler(req, res);
             break;
-        case "PUT":
         case "DELETE": request_handler.methodNotSupport(res, req);
             break;
         default: request_handler.errorRequest(res);
