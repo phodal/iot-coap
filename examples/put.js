b5d0f9cbbc668fd092fc36f2bc7f872154b1e7d5
Phodal Huang <h@phodal.com>
2014-08-19 16:56:12 +0800

    Add the PUT support

--- examples/put.js
+++ examples/put.js
@@ -11,10 +11,11 @@ var req = request({
 req.setHeader("Accept", "application/json");
 var bl = require('bl');
 
+req.setOption('Block2',  [new Buffer('3'),new Buffer('phodal'), new Buffer('13'), new Buffer('12')]);
 req.on('response', function(res) {
     res.pipe(bl(function(err, data) {
-        var json = JSON.parse(data);
-        console.log(json);
+//        var json = JSON.parse(data);
+        console.log(data);
         process.exit(0);
     }));
 
