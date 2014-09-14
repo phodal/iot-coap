3b6bc38ec82f5090c9879bdef4e6cfa26203902e
Phodal Huang <fengda.huang@rea-group.com>
2014-09-14 09:53:31 +0800

    add some method test

--- index.js
+++ index.js
@@ -4,6 +4,3 @@ var iotcoap         = require('./lib/coap_server');
 
 module.exports = iotcoap;
 module.exports.rest = rest;
-
-iotcoap.run();
-iotcoap.rest.run(); //运行REST
---
+++ test/function_test.js
@@ -0,0 +1,11 @@
+var iotcoap             = require('../lib/coap_server');
+
+describe('contact page', function() {
+    before(function() {
+        this.server = iotcoap;
+    });
+
+    it('should show contact a form');
+    it('should refuse empty submissions');
+
+});
