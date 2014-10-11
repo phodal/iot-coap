05ddfed0efc784e6745c1baf5a2b9fa11466eb89
Phodal HUANG <h@phodal.com>
2014-10-11 10:49:17 +0800

    Mongodb: add the simple mongodb get support

--- iot.js
+++ iot.js
@@ -1,5 +1,6 @@
 exports.config  = {
     "db_name": "iot.db",
+    "mongodb_name": "iot",
     "table_name": "basic",
     "keys":[
         "id",
@@ -8,6 +9,20 @@ exports.config  = {
         "sensors2"
     ],
     "db_table": "id integer primary key, value text, sensors1 float, sensors2 float",
+    "mongodb_init":[
+        {
+            id: 1,
+            value: "is id 1",
+            sensors1: 19,
+            sensors2: 20
+        },
+        {
+            id: 2,
+            value: "is id 2",
+            sensors1: 20,
+            sensors2: 21
+        }
+    ],
     "init_table":[
         "insert or replace into basic (id,value,sensors1,sensors2) VALUES (1, 'is id 1', 19, 20);",
         "insert or replace into basic (id,value,sensors1,sensors2) VALUES (2, 'is id 2', 20, 21);"
