44d743f719850b3011b956062fdfa8e7927eb830
Phodal HUANG <h@phodal.com>
2014-10-15 16:56:44 +0530

    Refactor the DB Handler

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 var coap            = require('coap');
 var rest            = require('./lib/rest_server');
 var iotcoap         = require('./lib/coap_server');
-var config          = require('./iot').config;
 var sqlite3         = require('sqlite3').verbose();
-var _               = require("underscore");
-var MongoDBHelper   = require('./lib/mongodb_helper');
-var SQLiteHelper    = require('./lib/sqlite_helper');
+var DBHandler       = require('./lib/db_handler');
 
-if (config.db === 'sqlite3') {
-    SQLiteHelper.init();
-} else if (config.db === "mongodb") {
-    MongoDBHelper.init();
-}
+DBHandler.init();
 
 module.exports =  iotcoap;
 module.exports.rest = rest;
