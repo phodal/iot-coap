cbaf203b6f6c8c826da1f4cea93339204b72892f
Phodal HUANG <h@phodal.com>
2014-10-18 09:43:01 +0530

    Add the URL test

---
+++ test/unit/url_test.js
@@ -0,0 +1,28 @@
+var URLHandler = require('../../lib/url_handler.js');
+
+describe('test url handler', function() {
+
+    var url = '/id/1';
+
+    it('should return \'id\' when get key from url', function() {
+        var result = URLHandler.getKeyFromURL(url);
+        expect(result).to.eql('id');
+    });
+
+    it('should return 1 when get key from url', function() {
+        var result = URLHandler.getValueFromURL(url);
+        expect(result).to.eql('1');
+    });
+
+    it('should return false when url no match', function() {
+        var url = "id";
+        var result = URLHandler.url_sanity_check(url);
+        expect(result).to.eql(false);
+    });
+
+    it('should return true when url match', function() {
+        var result = URLHandler.url_sanity_check(url);
+        expect(result).to.eql(true);
+    });
+
+});
