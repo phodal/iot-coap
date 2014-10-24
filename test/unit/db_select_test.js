335d94042ea94d5644d264e55d130635c236a306
Phodal HUANG <h@phodal.com>
2014-10-24 10:20:42 +0530

    Add the test coverage

---
+++ test/unit/db_select_test.js
@@ -0,0 +1,12 @@
+//var DB_Factory      = require("../../lib/database/db_factory");
+//var db_factory      = new DB_Factory();
+//var sinon           = require("sinon");
+//var stub            = sinon.stub().throws();
+//var database        = db_factory.selectDB();
+//
+//describe('SelectDB', function() {
+//    it('should throw error on errorHandler', function () {
+//        var config = stub.returns(4);
+//        console.log(config);
+//    });
+//});
