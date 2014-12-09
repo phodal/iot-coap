698922e00bf06e8e495eff70f7d1b7a2ab0276b4
Fengda HUANG <h@phodal.com>
2014-12-09 13:55:39 +0800

    move the init func to common.js

--- test/common.js
+++ test/common.js
@@ -1,3 +1,8 @@
 global.expect = require("chai").expect;
 global.assert = require("chai").assert;
 global.should = require('chai').should;
+
+var DB             =require("../lib/database/sqlite_helper")
+    ,sqlite        = new DB();
+
+sqlite.init();
