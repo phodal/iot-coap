56b43f1d2998bdffad502e1c97052a57018b57e0
Fengda HUANG <h@phodal.com>
2014-12-23 20:51:04 +0800

    set global mocha callback & remove chai global callback

--- test/common.js
+++ test/common.js
@@ -1,7 +1,6 @@
 global.expect = require("chai").expect;
 global.assert = require("chai").assert;
 global.should = require('chai').should;
-global.callback = require('chai').callback;
 
 var DB             =require("../lib/database/sqlite_helper")
     ,sqlite        = new DB();
